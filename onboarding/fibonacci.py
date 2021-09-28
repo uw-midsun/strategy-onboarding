ca9a13eaf18d060340a71ea69712ca7ece398dce
Daniel Qu <danielq987@gmail.com>
2021-09-28 13:37:50 -0400

    tabs/spaces formatting

--- onboarding/fibonacci.py
+++ onboarding/fibonacci.py
@@ -1,37 +1,37 @@
 def fibonacci_term(n):
-	'''
-	Finds nth term of the Fibonacci sequence from:
-		Term | value
-		------------
-		  1  |   1
-		  2  |   1
-		  3  |   2
-		  4  |   3
-		  5  |   5
-		 ... |  ...
+    '''
+    Finds nth term of the Fibonacci sequence from:
+            Term | value
+            ------------
+              1  |   1
+              2  |   1
+              3  |   2
+              4  |   3
+              5  |   5
+             ... |  ...
 
-	@params n: integer
-	@returns integer, nth term of the Fibonacci sequence
-	'''
-	lookup = {};
+    @params n: integer
+    @returns integer, nth term of the Fibonacci sequence
+    '''
+    lookup = {}
 
-	if n < 1:
-		raise TypeError("n must be a positive integer")
+    if n < 1:
+        raise TypeError("n must be a positive integer")
 
-	def compute_recursive(n):
-		'''
-		Recursive helper function
-		'''
+    def compute_recursive(n):
+        '''
+        Recursive helper function
+        '''
 
-		if (lookup.get(n)):
-			return lookup[n]
+        if (lookup.get(n)):
+            return lookup[n]
 
-		if (n <= 2):
-			return 1
-		
-		value = compute_recursive(n - 1) + compute_recursive(n - 2)
-		lookup[n] = value
+        if (n <= 2):
+            return 1
 
-		return value
+        value = compute_recursive(n - 1) + compute_recursive(n - 2)
+        lookup[n] = value
 
-	return compute_recursive(n)
+        return value
+
+    return compute_recursive(n)
--- onboarding/tests/test_array.py
+++ onboarding/tests/test_array.py
@@ -1,21 +1,26 @@
 import pytest
-from ..array import find_min_index, reverse_str_arr 
+from ..array import find_min_index, reverse_str_arr
 
 # write some tests here!
+
+
 def test_min_index():
-	assert(find_min_index([1, 33, 1, -2, 0]) == 3)
-	assert(find_min_index([0]) == 0)
-	assert(find_min_index([5, 4, 3, 1, 1]) == 3)
-	assert(find_min_index([0, 0, 0, 0, 0, 0]) == 0)
+    assert(find_min_index([1, 33, 1, -2, 0]) == 3)
+    assert(find_min_index([0]) == 0)
+    assert(find_min_index([5, 4, 3, 1, 1]) == 3)
+    assert(find_min_index([0, 0, 0, 0, 0, 0]) == 0)
+
 
 def test_min_index_empty():
-	with pytest.raises(ValueError):
-		find_min_index([])
+    with pytest.raises(ValueError):
+        find_min_index([])
+
 
 def reverse_str_arr():
-	assert(reverse_str_arr("abc") == ["c", "b", "a"])
-	assert(reverse_str_arr("abba") == ["a", "b", "b", "a"])
-	assert(reverse_str_arr("!!-.2") == ["2", ".", "-", "!", "!"])
+    assert(reverse_str_arr("abc") == ["c", "b", "a"])
+    assert(reverse_str_arr("abba") == ["a", "b", "b", "a"])
+    assert(reverse_str_arr("!!-.2") == ["2", ".", "-", "!", "!"])
+
 
 def reverse_str_arr_empty():
-	assert(reverse_str_arr("") == [])
+    assert(reverse_str_arr("") == [])
