4bb3a48b2162ed99a51549e2a357f24012610171
Matthew Erxleben <matthew.erxleben@uwaterloo.ca>
2022-01-22 16:32:10 -0500

    commiting fibonacci

--- onboarding/fibonacci.py
+++ onboarding/fibonacci.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 def fibonacci_term(n):
     '''
     Finds nth term of the Fibonacci sequence from:
@@ -14,5 +16,28 @@ def fibonacci_term(n):
     @returns integer, nth term of the Fibonacci sequence
     '''
 
-    # insert code here
-    pass
+    '''
+    Value error:
+      - Raised when a built-in operation or function receives an argument that has the right type but an inappropriate value
+      - the float function can take a string, ie float('5'), it's just that the value 'string' in float('string') is an inappropriate (non-convertible) string
+
+    Type error:
+      - Passing arguments of the wrong type (e.g. passing a list when an int is expected) should result in a TypeError
+      - so you would get a TypeError if you tried float(['5']) because a list can never be converted into a float.
+    '''
+
+    if n < 1 or type(n) != int:
+      raise TypeError
+    
+    prev = 0
+    current = 1
+
+    if n == 1 :
+      return 1
+    
+    # starts at 2nd term
+    for _ in range(n - 1) : 
+      temp = prev
+      prev = current
+      current += temp
+    return current
