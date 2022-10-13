849c0aee276602b0bcef859ab9d587e1be663dfe
Vinayak Bector <69417893+VinayakBector2002@users.noreply.github.com>
2022-10-13 21:16:55 +0000

    fibonacci added

--- onboarding/fibonacci.py
+++ onboarding/fibonacci.py
@@ -1,6 +1,6 @@
 def fibonacci_term(n):
     '''
-    Finds nth term of the Fibonacci sequence from:
+		Finds nth term of the Fibonacci sequence from:
         Term | value
         ------------
           1  |   1
@@ -9,10 +9,13 @@ def fibonacci_term(n):
           4  |   3
           5  |   5
          ... |  ...
-
-    @params n: integer
-    @returns integer, nth term of the Fibonacci sequence
+    	@params n: integer
+    	@returns integer, nth term of the Fibonacci sequence
     '''
-
-    # insert code here
-    pass
+    def fib(n):
+        if (n in (0,1)):
+            return n
+        return fib(n-1) + fib(n-2)
+    if (isinstance(n,int) and n>=1):
+        return fib(n)
+    raise TypeError
