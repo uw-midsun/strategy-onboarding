7617c8a63b03bd56545031afdfc1564347568074
RenJieZheng <42659888+RenJieZheng@users.noreply.github.com>
2023-01-17 23:45:47 -0500

    completed matplot

--- onboarding/matplot.py
+++ onboarding/matplot.py
@@ -1,7 +1,26 @@
-'''
-TODO
-- Graph all the data. This can be all the same plot, or four separate plots
-    Use Matplotlib, please!
-- Be careful of data points that don't exist
-- Will require reading from CSV, can experiment with pandas
-'''
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import data
+data = pd.read_csv("test_data.csv", header=None)
+
+plt.figure(1)
+plt.plot(data[0])
+
+plt.figure(2)
+plt.plot(data[1])
+
+plt.figure(3)
+plt.plot(data[2])
+
+plt.figure(4)
+plt.yscale("log")
+plt.plot(data[3])
+
+plt.figure(5)
+ax = plt.axes(xscale="linear", yscale="log")
+ax.yaxis.set_major_locator(plt.MaxNLocator(10))
+plt.plot(data[4])
+
+plt.show()
