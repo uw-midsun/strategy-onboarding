9d5c90ce56224329b2ce2ee1a41ce73a0481b0bd
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-03-25 19:03:30 -0400

    rename dirs

---
+++ task2_coding_exercises/tests/test_q1_arrays.py
@@ -0,0 +1,24 @@
+import pytest
+
+from ..q1_arrays import find_min_index, reverse_str_arr, most_freq_element
+
+
+# def test_find_min_index():
+#     assert find_min_index([1, 33, 1, -2, 0]) == 3
+#     assert find_min_index([0, 0, 0, 0, 0]) == 0
+#     assert find_min_index([100]) == 0
+
+
+# def test_reverse_str_arr():
+#     assert reverse_str_arr("abc") == ["c", "b", "a"]
+#     assert reverse_str_arr("abba") == ["a", "b", "b", "a"]
+#     assert reverse_str_arr("!!-.2") == ["2", ".", "-", "!", "!"]
+
+
+# def test_most_freq_element():
+#     assert most_freq_element([1, 33, 1, -2, 33]) == 33
+#     assert most_freq_element([1, 2, 3, 4]) == 1
+#     assert most_freq_element([3, 5, 1, 5, 3]) == 3
+
+if __name__ == "__main__":
+    print("gi")
