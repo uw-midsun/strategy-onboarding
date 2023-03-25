c9ad2efb02f16cbcf9a66263e95f288652b114c2
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-03-25 18:15:02 -0400

    task 2 q3

---
+++ Task 2 - Coding exercises/q3_data_science.py
@@ -0,0 +1,10 @@
+"""
+We've put together some very simple data in "q3_test_data.csv". 
+Read it and graph it using Matplotlib. This can be all the same plot or four separate plots.
+Put the generated plots in the "q3_plots" folder for us to see when you make a pull request
+
+- Be careful of data points that don't exist
+- Will require reading from CSV so feel free to use pandas
+"""
+
+# Your code here
