dfe33327de115de10579791fe9fecec67362a313
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2023-03-25 19:05:34 -0400

    test cases

--- task2_coding_exercises/tests/test_q2_debugging.py
+++ task2_coding_exercises/tests/test_q2_debugging.py
@@ -1,21 +1,18 @@
-# import sys, os
+import pytest
 
-# sys.path.insert(0, os.path.abspath(".."))
-
-# import pytest
-# from ..q2_debugging import adjacent_subtraction, str_math
+from ..q2_debugging import adjacent_subtraction, str_math
 
 
-# def test_adjacent_subtraction():
-#     assert adjacent_subtraction([1, 6, 3, 2, 8]) == [5, -3, -1, 6]
-#     assert adjacent_subtraction([1, 2, 3, 4]) == [1, 1, 1, 1]
-#     assert adjacent_subtraction([1]) == [1]
-#     assert adjacent_subtraction([]) == None
+def test_adjacent_subtraction():
+    assert adjacent_subtraction([1, 6, 3, 2, 8]) == [5, -3, -1, 6]
+    assert adjacent_subtraction([1, 2, 3, 4]) == [1, 1, 1, 1]
+    assert adjacent_subtraction([1]) == [1]
+    assert adjacent_subtraction([]) == None
 
 
-# def test_str_math():
-#     assert str_math(["1", "6", "3", "2", "8"]) == 20
-#     assert str_math(["1", "b", "3", "-2", []]) == 2
-#     assert str_math(["1", "b", "3.5", "-12"]) == -7.5
-#     assert str_math(["hi"]) == None
-#     assert str_math([]) == None
+def test_str_math():
+    assert str_math(["1", "6", "3", "2", "8"]) == 20
+    assert str_math(["1", "b", "3", "-2", []]) == 2
+    assert str_math(["1", "b", "3.5", "-12"]) == -7.5
+    assert str_math(["hi"]) == None
+    assert str_math([]) == None
